d100c8bd05d9d890fa9901830b00384f220af098
Ajit Durgam <durgamajit11@gmail.com>
2026-04-09 12:13:53 +0530

    Update secondlargest.java

--- secondlargest.java
+++ secondlargest.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class secondlargest {
     public static void main(String[] args) {
       // System.out.println("Hello, World!");
       int[] arr = {10,20,4,45,99};
